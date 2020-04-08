baee85d86401d39aaabf93c20b95e26cbb7e4735
Caden <cadence1547@gmail.com>
2020-04-08 16:48:13 -0700

    first commit

---
+++ todo-demo/App.js
@@ -0,0 +1,80 @@
+import React,{useState}from 'react';
+
+import { StyleSheet, Text, View, NativeAppEventEmitter, Button, TextInput,FlatList } from 'react-native';
+import Header from './components/header';
+
+
+export default function App(){
+  const [todos,setTodos]=useState([
+    {text:'buy coffee', key:"1"},
+    {text:'create an app',key:'2'},
+    {text:'play on the swith', key:'3'}
+  ]);
+
+  return (
+    <View style={styles.container}>
+      <Header/>
+      <View style={styles.content}>
+        <View style={styles.list}>
+  <FlatList data={todos} renderItem={({item})=>(TodoItem item={item})}/>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+const styles=StyleSheet.create({
+  container:{
+    flex:2,
+    backgroundColor:'#fff',
+  },
+  content:{
+    padding:40,
+  },
+  list:{
+    marginTop:20,
+  }
+
+})
+
+// export default function App(){
+//    const [textInput,setTextInput]=useState('');
+//    const [todos,setTodos]=useState([]);
+  
+//    const pressHandler=()=>{
+//     setTodos([textInput,...todos])
+//    }
+//    const typingHandler=(value)=>{
+//     setTextInput(value);
+//  }
+//     return(
+//       <View style={styles.container}>
+//         <Text>Todo App</Text>
+//         <TextInput style={{borderWidth:1,height:50, width:"100%"}} onChangeText={typingHandler} value={textInput}/>
+//         <Button title="Submit" onPress={pressHandler}/>
+//         {/* <FlatList data={todos} renderItem={todo=><Todo text={todo.item}/>}/> */}
+//         <FlatList data={todos} renderItem={todo=><Todo text1={todo.item}/>} />
+//       </View>
+//     );
+  
+// }
+
+// const Todo=props=>(
+//   <View style={{backgroundColor:'#eaeaea',width:300,margin:5}}>
+//     <Text>{props.text1}</Text>
+//     {console.log(props.text)}
+//   </View>
+// );
+// const styles = StyleSheet.create({
+//   container: {
+//     marginTop:50,
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+    
+//   }
+// });
+
+
+
