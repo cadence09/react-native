ec541935396e79ff0a6358144c389922833d0b86
paolitaclo <paolita.claros@gmail.com>
2020-04-13 21:07:56 -0700

    Create eslintrc file and updated files with eslint

--- chat/screens/camera.js
+++ chat/screens/camera.js
@@ -1,42 +1,49 @@
-import React, {useState, useEffect} from 'react';
-import {Camera} from 'expo-camera';
-import {View,TouchableOpacity,Text,StyleSheet} from 'react-native';
-import {Ionicons,MaterialIcons} from '@expo/vector-icons';
+import React, { useState, useEffect } from 'react';
+import { Camera } from 'expo-camera';
+import {
+  View, TouchableOpacity, Text, StyleSheet,
+} from 'react-native';
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
 
-export default function TakePhoto (){
-    const [cameraType,setCameraType]=useState(Camera.Constants.Type.back);
-    const [hasPermission,setHasPermission]=useState(null);
-    // useEffect(() => {
-    //     (async () => {
-    //       const { status } = await Camera.requestPermissionsAsync();
-    //       setHasPermission(status === 'granted');
-    //     })();
-    //   }, []);
-return (
-    <View>
-        <Camera style={styles.Camera} type={cameraType} ref={ref=>{setCameraType(ref)}}>
+export default function TakePhoto() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <Camera style={styles.Camera} type={cameraType}>
         <View>
-            <TouchableOpacity>
-                <Ionicons name='md-folder-open'></Ionicons>
-            </TouchableOpacity>
-            <TouchableOpacity>
-                <MaterialIcons name='linked-camera'></MaterialIcons>
-            </TouchableOpacity>
-            <TouchableOpacity>
-            <Ionicons name='ios-reverse-camera'></Ionicons>
-            </TouchableOpacity>
-         </View>
-  
-        </Camera>
+          <TouchableOpacity>
+            <Ionicons name="md-folder-open" />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <MaterialIcons name="linked-camera" />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Ionicons name="ios-reverse-camera" />
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
-)
+  );
 }
 
-const styles=StyleSheet.create({
-  Camera:{
-      flex:1,
-      backgroundColor:'yellow',
-  }
-})
-
-
+const styles = StyleSheet.create({
+  Camera: {
+    flex: 1,
+    backgroundColor: 'yellow',
+  },
+});
